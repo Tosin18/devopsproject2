4bab1f102f933ce7096bb850bcbf17881f810676
Tee <tolowoye@tebtechsolutions.com>
2020-07-02 23:00:27 +0100

    aws code update

--- main.tf
+++ main.tf
@@ -6,42 +6,11 @@ resource "aws_key_pair" "tkey" {
 resource "aws_instance" "devopstrain" {
   ami           = var.ami_id
   instance_type = "t2.micro"
-  vpc_security_group_ids = var.vpc_security_group_ids
-  subnet_id = var.subnet_id
-  key_name = "tkey"
+  key_name      = "tkey"
 
   tags = {
     Name = "DevOps Build"
   }
-
-  resource "aws_security_group" "security_group" {
-  name = "security"
-  vpc_id = var.aws_vpc.vpc.id
-  ingress {
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-      from_port   = 80
-      to_port     = 80
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-    egress {
-    from_port   = 8081
-    to_port     = 8081
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-
-}
 
   provisioner "remote-exec" {
     connection {
@@ -57,9 +26,12 @@ resource "aws_instance" "devopstrain" {
       "sudo yum update -y",
       "sudo yum install java-1.8.0-openjdk -y ",
       "sudo yum install maven -y",
-      ]
-
-    }
+      "sudo amazon-linux-extras enable nginx1.12",
+      "yum clean metadata",
+      "yum install nginx",
+      "sudo yum install nginx -y",
+      "sudo systemctl start nginx",
+    ]
 
   }
 
--- variable.tf
+++ variable.tf
@@ -13,14 +13,12 @@ variable "dir_to_private_key" {
   default = "tkey"
 }
 
-variable "subnet_id"{ 
-    default = "subnet-d7eb848d" 
-    }
+variable "aws_subnet_public_id" { default = "subnet-d7eb848d" }
 
-variable "vpc_security_group_ids" {
-    default = "sg-f0879abe"
-  }
+variable "vpc_security_group" {
+  default = "vpc_security_group_id"
+}
 
-variable "vpc_id_vpc_id" { 
-    default = "vpc-14cb3b6d" 
-    }
+variable "vpc_id_vpc_id" {
+  default = "vpc-14cb3b6d"
+}
