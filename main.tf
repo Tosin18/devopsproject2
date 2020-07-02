ae2a745b470bc5a31e17d0168cb85396eb270d85
Tee <tolowoye@tebtechsolutions.com>
2020-07-02 15:45:50 +0100

    main.tf commitedi

--- main.tf
+++ main.tf
@@ -1,17 +1,16 @@
-resource "aws_key""tkey"{
-    key_name = "tkey"
-    public_key = file(var.dir_to_public_key)
-
-}
-resource "aws_instance" "devopsproj"{
-    ami  = var.ami_id
-    instance_type = "t2.micro"
-    key_name= "tkey"
+resource "aws_key_pair" "tkey" {
+  key_name   = "tkey"
+  public_key = file(var.dir_to_public_key)
 }
 
-tags "DevOps" ={
+resource "aws_instance" "devopstrain" {
+  ami           = var.ami_id
+  instance_type = "t2.micro"
+  key_name      = "tkey"
+
+  tags = {
     Name = "DevOps Build"
-}
+  }
 
   provisioner "remote-exec" {
     connection {
@@ -20,14 +19,13 @@ tags "DevOps" ={
       host        = aws_instance.devopstrain.public_ip
       private_key = file(var.dir_to_private_key)
     }
-     inline = [
+    inline = [
       "sudo yum update -y",
       "sudo yum install git -y",
       "sudo yum update -y",
       "sudo yum install java-1.8.0-openjdk -y ",
       "sudo yum install maven -y",
-     ]
- 
-   } 
- 
+    ]
+
   }
+}
